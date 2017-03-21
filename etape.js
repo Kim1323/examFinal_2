c0401430b71582fe6abe7500d8174db9467de14e
My Kim Duong <mykimduong13@gmail.com>
2017-03-21 16:19:40 -0400

    Etape#1 - Deboggage + Etape#2 - Creation du loop for dans le ejs

--- etape.js
+++ etape.js
@@ -9,10 +9,8 @@ app.use(bodyParser.urlencoded({extended: true}))
 app.use(express.static('public'))  // pour utiliser le dossier public
 app.use(bodyParser.json())  // pour traiter les données JSON
 
-var http = require('http');
 var url = require('url');
 var obj;
-
 
 var db // variable qui contiendra le lien sur la BD
 
@@ -26,17 +24,22 @@ MongoClient.connect('mongodb://127.0.0.1:27017/carnet_adresse', (err, database) 
 
 
 app.get('/',  (req, res) => {
-   console.log('la route route get / = ' + req.url)
-    var cursor = db.collection('provinces').find().toArray(function(err, resultat){
+   console.log("allo")
+    db.collection('provinces').find().toArray(function(err, resultat){
        if (err) return console.log(err)
     res.render('index.ejs', {provinces: resultat})
-    }) 
+    })
 })
 
-
 app.get('/fichier',  (req, res) => {
-	fs.readFile('public/text/collection_provinces.json', 'utf8', function (err, data) {
-	  if (err) throw err;
-	  obj = JSON.stringify(data);
-	});
+	console.log("fichier");
+   	res.sendFile(__dirname + "/public/text/collection_provinces.json")
+})
+
+app.get('/provinces',  (req, res) => {
+	console.log("provinces");
+})
+
+app.get('/collection',  (req, res) => {
+	console.log("collection");
 })
